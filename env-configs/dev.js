3944b3971eee52091b10e81d77aeb4a1d2c65f57
anzhela.potapchuk <anzhela.potapchuk@globallogic.com>
2021-05-27 23:41:21 +0300

    BaseConfig updated

--- env-configs/dev.js
+++ env-configs/dev.js
@@ -1,27 +1,48 @@
 class Config(BaseConfig):
-def__init__(self):
-config_path = f"env_configs/{os.enwiron..."
-if os.environ["TARGET"] == "prod"
-    else "specifications/scout_index/dev.y..."
-)
+def__init__(self);
+config_path = f"env_configs/{os.enwiron...";
+if(os.environ["TARGET"]=="prod"):
+
+    else "specifications/scout_index/dev.y..."=
+(
 app_env = ConfigFromEnvProvider()
-app_json_conf = ConfigFromSimpleJsonProvid...
-app_defaults = ConfigFromDictProvider(
-"USERS_DATA_PATH": "data/user..."
-"SAMPLES_DATA_PATH": "data/sampl..."
-"SCOUT_DATA_PATH": f"specifications/workf..."
-"SCOUT_SPECS_PATH": "scout_spec_path",
-"AWS_UPLOADS_BUCKET": "cosm..."
-"AWS_SECRET_ACCESS_KEY:"
-"AWS_ACCESS_KEY_ID":"..."
-"AWS_DEFAULT_REGION": "US -..."
-"AWS_S3_ENDPOINT_URL": "https://..."
-"ADMIN_BASE_URL": "https://..."
-"UTOOL_BASE_URL":
-"CLI_BASE_URL":
-"CLI_INSTALL_PARAMS":
-"CLI_VERSION":
-"SCOUT_BASE_URL":
-"ILLUMINA_CLIENTT_..."
-"REDIS_PORT": "6379" +
-"REDIS_HOST": ".."
+app_json_conf = ConfigFromSimpleJsonProvider/cofig()
+app_defaults = ConfigFromDictProvider()
+{
+    "USERS_DATA_PATH":"test_data/users";
+    "SAMPLES_DATA_PATH":"test_data/samples";
+    "SCOUT_DATA_PATH":"test_data/scout/search_qu...";
+    "WORKFLOW_SPECS_PATH":f"specifications/workflow";
+    "SCOUT_SPECS_PATH":"scout_spec_path";
+    "GMAIL_IMAP_HOST":"imap.gmail.com";
+    "EXPIRES":86400;
+    "LOGGING_COLORED":"1";
+    "AUTO_SAMPLES_FOLDER":"origin_files/automation";
+    "GRID_HUB_URL":"http://selenium.grid.dev.cosmosid"
+    }
+)
+super(config,self).init()
+{
+    "AWS_UPLOAD_BUCKET: cosmosid-samples";
+    "AWS_SECRET_ACCESS_KEY: ExBf32eW6...";
+    "ASW_ACCESS_KEY_ID: AKIA...";
+    "AWS_DEFAULT_REGION: US-east-1";
+    "AWS_S3_ENDPOINT_URL: https://s3.amazonaws.com";
+    "AWS_APP_BASE_URL: https://www-int.cosmosid.com";
+    "ADMIN_BASE_URL: https://www-int.cosmosid.com.8443";
+    "UTOOL_BASE_URL: https"//base.cid.int:88";
+    "CLI_BASE_URL: https://rest-int.cosmosid.com";
+    "CLI_INSTALL_PARAMS: --pre --extra-index-url https://pip...";
+    "CLI_VERSION:"";
+    "SCOUT_BASE_URL: https://compute.cid.int:9004/";
+    "ILLUMINA_CLIENT_KEY: 7eab2b89...";
+    "REDIS_PORT: 6379";
+    "REDIS_HOST: ec-cid-int.86dif0.001.usel...";
+    "DB_HOST: rds-int...";
+    "DB_PORT: 5432";
+    "DB_NAME: cid_int";
+    "DB_USER: cid_int";
+    "DB_PASS: eewisha2D...";
+    "DB_CUSTOMER: test@automation.ua"
+    }
+}
